a7ff9aa48bb6ba5854f22f421f02bd4068d4f8ee
Ifrago <ivan.f.a@hotmail.com>
2016-03-13 21:07:41 +0100

    Token activate (disabled)

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../private/database');
 var mongoose = require('mongoose');
 var modelUser = require('../models/users.js');
-var modelXBee = require('../models/xbee.js');
+var jwt = require('jsonwebtoken');
+var fs= require('fs');
+var privKey =fs.readFileSync('private/server.key');
+var pubKey = fs.readFileSync('private/server.pub');
+
 
 //Peticiones HTTP REVISAR AL FINAL CUANDO TODO FUNCIONE  USAR TOKETNS
 //GET---------------------
@@ -16,7 +19,7 @@ router.get('/all', function(req,res,next){//Get All Users
     })
 });
 router.get('/:username', function(req,res,next){//GET User
-    modelUser.find({username:req.params.username}, function(err,user){
+    modelUser.findOne({username:req.params.username}, function(err,user){
         if (!err) {
             console.log('User: '+user);
             res.jsonp(user);
@@ -28,66 +31,96 @@ router.get('/:username', function(req,res,next){//GET User
 //POST---------------------
 router.post('/',function(req,res,next){//Register User
   if(req.body.username=="" || req.body.userpass=="")  res.jsonp({message:"Hay campos obligaotrios vacios"});
-  else{
-      if(modelUser.find({username:req.body.username}, function(err,user){
-              if(!err) return user;
+  else {
+      modelUser.findOne({username: req.body.username}, function (err, user) {
+              if (!err) {
+                  if (user == null) {
+                      modelUser.create({username: req.body.username, userpass: req.body.userpass}, function (err, user) {
+                          if (err) res.jsonp(err);
+                          res.status(200).json({
+                              user: user,
+                              token:jwt.sign(user.username, privKey, {algorithm: 'RS256'})
+                          });
+                      });
+                  } else return res.status(409).json({message: 'Exist User'});
+              }
               else res.jsonp(err);
-          })) return res.status(409).json({message: 'Exist User'});
-      else{
-          modelUser.create({username: username, userpass: password},function(err,user){
-              if(err) res.jsonp(err);
-              res.status(200).json(user);
-          });
-
-      }
-
+          }
+      );
   }
-});
-//PUT-----------------------
-router.put('/:username', function(req,res,next){//Modify User(userpass &/or xbeepan)
-    if(req.body.userpass!="" && req.body.xbeepan!="") {
-        modelUser.update({username: req.params.username}, {$set: {userpass: req.body.userpass, xbeepan: req.body.xbeepan}}, function (err, user) {
-            if (err) res.status(500).json(err);
-            res.status(200).json(user);
-        });
-    }else if(req.body.userpass=="" && req.body.xbeepan!="") {
-        modelUser.update({username: req.params.username}, {$set: {xbeepan: req.body.xbeepan}}, function (err, user) {
-            if (err) res.status(500).json(err);
-            res.status(200).json(user);
-        });
-    }else if(req.body.userpass!="" && req.body.xbeepan=="") {
-        modelUser.update({username: req.params.username}, {$set: {userpass: req.body.userpass}}, function (err, user) {
-            if (err) res.status(500).json(err);
-            res.status(200).json(user);
-        });
-    }else res.status(400).json({message:"Campos para cambiar vacios"});
-});
-router.put('/signin',function(req,res,next){//SIGNIN User
+});//Register User
+router.post('/:username', function(req,res,next) {//Modify User(userpass &/or xbeepan)
+    try {
+           var decoded = jwt.verify(req.body.token, pubKey);
+            console.log(" Token decoded :" + decoded + " Username: "+req.params.username);
+            if (err) res.status(401).json(err);
+            if(decoded==req.params.username){
+                if (req.body.userpass != "" && req.body.xbeepan != "") {
+                    modelUser.update({username: req.params.username}, {
+                        $set: {
+                            userpass: req.body.userpass,
+                            xbeepan: req.body.xbeepan
+                        }
+                    }, function (err, user) {
+                        if (err) res.status(500).json(err);
+                        res.status(200).json(user);
+                    });
+                } else if (req.body.userpass == "" && req.body.xbeepan != "") {
+                    modelUser.update({username: req.params.username}, {$set: {xbeepan: req.body.xbeepan}}, function (err, user) {
+                        if (err) res.status(500).json(err);
+                        res.status(200).json(user);
+                    });
+                } else if (req.body.userpass != "" && req.body.xbeepan == "") {
+                    modelUser.update({username: req.params.username}, {$set: {userpass: req.body.userpass}}, function (err, user) {
+                        if (err) res.status(500).json(err);
+                        res.status(200).json(user);
+                    });}
+            } else res.status(400).json({messageModify: "Unauthoritzed, token is old"});
+    }catch(err){
+        res.status(401).json({err: err, messageModify: "Unauthoritzed, token is invalid"});
+    }});//Modify User(userpass &/or xbeepan)
+
+router.post('/signin',function(req,res,next){//SIGNIN User
     console.log("MESSAGE- Username: "+req.body.username+" Userpass: "+ req.body.userpass);
     modelUser.findOne({username: req.body.username}, function (err, user) {
         if(err) res.status(500).json(err);
         console.log("DATABASE- Username: "+user.username+" Userpass: "+ user.userpass+ " User "+ user);
         if(req.body.username == user.username){
             if(req.body.userpass== user.userpass){
-                res.status(200).json(user);
+                res.status(200).json({
+                    user:user,
+                    token:jwt.sign(user.username, privKey, {algorithm: 'RS256'})
+                });
             }else res.status(401).json({message:"Userpass Wrong!"});
         }else res.status(404).json({message:"User not found in our database"});
     })
-});
+});//SIGNIN User
+
 //DELETE---------------------
 router.delete('/:username', function(req,res,next){//Dar de baja a un usuario (DELETE User)
-    modelUser.findOne({username: req.params.username}, function (err, user) {
-        if(err) res.status(500).json(err);
-        if(user==null) res.status(404).json({message:"User not found in our database"});
-        else  if(req.params.username == user.username){
-            console.log("DATABASE- Username: "+user.username+" Userpass: "+ user.userpass+ " User "+ user);
-            if(req.body.userpass== user.userpass){
-                modelUser.remove({username: req.params.username}, function(err){
-                    if(err) res.status(500).json(err);
-                    else res.status(200).json({message: 'Success!'});
-                })
-            }else res.status(401).json({message:"Userpass Wrong!"});
-        }
-    })
-});
+    try{
+        var decoded = jwt.verify(req.body.token, pubKey);
+        console.log(" Token decoded :" + decoded);
+        if(decoded==req.params.username){
+        modelUser.findOne({username: req.params.username}, function (err, user) {
+            if(err) res.status(500).json(err);
+            if(user==null) res.status(404).json({message:"User not found in our database"});
+            else  if(req.params.username == user.username){
+                console.log("DATABASE- Username: "+user.username+" Userpass: "+ user.userpass+ " User "+ user);
+                if(req.body.userpass== user.userpass){
+                    modelUser.remove({username: req.params.username}, function(err){
+                        if(err) res.status(500).json(err);
+                        else res.status(200).json({messageDelete: 'Success!'});
+                    })
+                }else res.status(401).json({messageDelete:"Userpass Wrong!"});
+            }
+        })
+        }else res.status(401).json({messageDelete:"Unauthoritzed"});
+    }catch(err){
+        res.status(401).json({err: err, messageDelete: "Unauthoritzed"});
+    }
+
+});//Dar de baja a un usuario (DELETE User)
+
+
 module.exports = router;
--- routes/xbees.js
+++ routes/xbees.js
@@ -3,9 +3,11 @@
  */
 var express = require('express');
 var router = express.Router();
-var db = require('../private/database');
 var mongoose = require('mongoose');
 var modelXBee = require('../models/xbee.js');
+var jwt = require('jsonwebtoken');
+var fs= require('fs');
+var pubKey = fs.readFileSync('private/server.pub');
 
 //Peticiones HTTP REVISAR AL FINAL CUANDO TODO FUNCIONE  USAR TOKETNS
 
@@ -29,71 +31,118 @@ router.get('/:mac', function(req,res,next){//GET XBee
 });
 
 //POST----------------------
-router.post('/', function(req,res,next){//Dar de Alta un XBeePAN
-    modelXBee.findOne({mac:req.body.mac}, function(err,xbee){
-            if(!err){
-                console.log("XBeePAN: "+xbee);
-                if(xbee!=null)  res.status(409).json({message: 'Exist XBee'});
-                else{
-                    modelXBee.create({mac: req.body.mac, owner: req.body.owner},function(err,xbee){
-                        if(err) res.jsop(err);
-                        res.status(200).json(xbee);
-                    });
+router.post('/', function(req,res,next) {//Dar de Alta un XBeePAN
+    try {
+        var decoded = jwt.verify(req.body.token, pubKey);
+        console.log(" Token decoded :" + decoded);
+        if (decoded == req.params.username) {
+            modelXBee.findOne({mac: req.body.mac}, function (err, xbee) {
+                if (!err) {
+                    console.log("XBeePAN: " + xbee);
+                    if (xbee != null)  res.status(409).json({message: 'Exist XBee'});
+                    else {
+                        modelXBee.create({mac: req.body.mac, owner: req.body.owner}, function (err, xbee) {
+                            if (err) res.jsop(err);
+                            res.status(200).json(xbee);
+                        });
+                    }
                 }
-            }
-            else res.json(err);
-        })
+                else res.json(err);
+            })
+        } else res.status(401).json({message: "Unauthoritzed"});
 
+    } catch (err){res.status(401).json({err:err,message: "Unauthoritzed"});}
 
-});
+});//Dar de Alta un XBeePAN
 router.post('/:mac', function(req,res,next){//Dar de Alta un XBeeNet
-    modelXBee.findOne({mac:req.params.mac}, function(err,xbee){
-            if(!err){
-                modelXBee.update({mac: req.params.mac}, {$addToSet: {xbeenet:req.body.xbeenet}}, function (err, xbee) {
-                    if (err) res.status(500).json(err);
-                });
-                modelXBee.findOne({mac: xbee.mac}, function(err,xbee){
-                    if (err) res.status(500).json(err);
-                    res.status(200).json(xbee);
-                });
-            }
-            else res.json(err);
-        })
 
-});
+   try{
+       var decoded = jwt.verify(req.body.token, pubKey);
+       console.log(" Token decoded :" + decoded);
+       if (decoded == req.params.username) {
+           modelXBee.findOne({mac:req.params.mac}, function(err,xbee){
+                    if(!err){
+                        modelXBee.update({mac: req.params.mac}, {$addToSet: {xbeenet:req.body.xbeenet}}, function (err, xbee) {
+                            if (err) res.status(500).json(err);
+                        });
+                        modelXBee.findOne({mac: xbee.mac}, function(err,xbee){
+                            if (err) res.status(500).json(err);
+                            res.status(200).json(xbee);
+                        });
+                    }else res.json(err);
+                })
+       }else res.status(401).json({message: "Unauthoritzed"});
+
+   }catch (err){res.status(401).json({err:err,message: "Unauthoritzed"});}
+});//Dar de Alta un XBeeNet
 
 //PUT-----------------------------
 router.put('/:mac/history', function(req,res,next){//Añadir registro de historial
-    modelXBee.findOne({mac:req.params.mac}, function(err,xbee){
-            if(!err) {
-                modelXBee.update({mac: req.params.mac}, {$addToSet: {history: req.body.history}}, function (err, xbee) {
-                    if (err) res.status(500).json(err);
-                });
-                modelXBee.findOne({mac: xbee.mac}, function(err,xbee){
-                    if (err) res.status(500).json(err);
-                    res.status(200).json(xbee);
-                });
-            }
-            else res.status(500).json(err);
-        })
-
-});
+    try{
+        var decoded = jwt.verify(req.body.token, pubKey);
+        console.log(" Token decoded :" + decoded);
+        if (decoded == req.params.username) {
+            modelXBee.findOne({mac:req.params.mac}, function(err,xbee){
+                    if(!err) {
+                        modelXBee.update({mac: req.params.mac}, {$addToSet: {history: req.body.history}}, function (err, xbee) {
+                            if (err) res.status(500).json(err);
+                        });
+                        modelXBee.findOne({mac: xbee.mac}, function(err,xbee){
+                            if (err) res.status(500).json(err);
+                            res.status(200).json(xbee);
+                        });
+                    }
+                    else res.status(500).json(err);
+                })
+        }else res.status(401).json({message: "Unauthoritzed"});
+    }catch (err){res.status(401).json({err:err,message: "Unauthoritzed"});}
+});//Añadir registro de historial
 
 //DELETE-------------------------------
 router.delete('/:mac', function(req,res,next){//Dar de baja XBeePAN
-    modelXBee.findOne({mac:req.params.mac}, function(err,xbee){
-        if(!err){
-            if(xbee.owner==req.body.owner){
-                console.log("Eliminando...");
-                modelXBee.remove({mac: req.params.mac}, function(err){
-                    if(err) res.status(500).json(err);
-                    else {
-                        console.log("Eliminado");
-                        res.status(200).json({message: 'Success!'});
-                    }
-                })
-            }else res.status(401).json({message:"You aren't owner this XBee"});
-        }else res.status(500).json(err);
-    });
-});
+    try{
+        var decoded = jwt.verify(req.body.token, pubKey);
+        console.log(" Token decoded :" + decoded);
+        if (decoded == req.params.username) {
+            modelXBee.findOne({mac:req.params.mac}, function(err,xbee){
+                if(!err){
+                    if(xbee.owner==req.body.owner){
+                        console.log("Eliminando...");
+                        modelXBee.remove({mac: req.params.mac}, function(err){
+                            if(err) res.status(500).json(err);
+                            else {
+                                console.log("Eliminado");
+                                res.status(200).json({message: 'Success!'});
+                            }
+                        })
+                    }else res.status(401).json({message:"You aren't owner this XBee"});
+                }else res.status(500).json(err);
+            });
+        }else res.status(401).json({message: "Unauthoritzed"});
+    }catch (err){res.status(401).json({err:err,message: "Unauthoritzed"});}
+});//Dar de baja XBeePAN
+router.delete('/:mac/xbeenet', function(req,res,next){//Dar de baja XBeeNET
+    try{
+        var decoded = jwt.verify(req.body.token, pubKey);
+        console.log(" Token decoded :" + decoded);
+        if (decoded == req.params.username) {
+            modelXBee.findOne({mac:req.params.mac}, function(err,xbee){
+                if(!err){
+                    if(xbee.owner==req.body.owner){
+                        console.log("Eliminando...");
+                        modelXBee.update({mac: req.params.mac}, {$pop: {xbeenet: req.body.xbeenet}}, function(err){
+                            if(err) res.status(500).json(err);
+                            else {
+                                console.log("Eliminado");
+                                res.status(200).json({message: 'Success!'});
+                            }
+                        });
+                    }else res.status(401).json({message:"You aren't owner this XBee"});
+                }else res.status(500).json(err);
+            });
+        }else res.status(401).json({message: "Unauthoritzed"});
+    }catch (err){res.status(401).json({err:err,message: "Unauthoritzed"});}
+});//Dar de baja XBeeNET
+
+
 module.exports = router;
